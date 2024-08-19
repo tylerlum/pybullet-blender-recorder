5f4357bb13ef004693f6e2189e65e7f8b71a39a0
Tyler Lum <tylergwlum@gmail.com>
2024-08-19 14:52:09 -0700

    Update import obj API

--- pyBulletSimImporter.py
+++ pyBulletSimImporter.py
@@ -63,9 +63,10 @@ class ANIM_OT_import_pybullet_sim(Operator, ImportHelper):
                         ".")[-1].lower()
                     # Handle different mesh formats
                     if 'obj' in extension:
-                        bpy.ops.import_scene.obj(
-                            filepath=pybullet_obj['mesh_path'],
-                            axis_forward='Y', axis_up='Z')
+                        bpy.ops.wm.obj_import(filepath=pybullet_obj['mesh_path'], forward_axis='Y', up_axis='Z')
+                        # bpy.ops.import_scene.obj(
+                        #     filepath=pybullet_obj['mesh_path'],
+                        #     axis_forward='Y', axis_up='Z')
                     elif 'dae' in extension:
                         bpy.ops.wm.collada_import(
                             filepath=pybullet_obj['mesh_path'])
