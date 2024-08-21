c3dd9169a3443a766a70a39914f64883fc522fe8
Tyler Lum <tylergwlum@gmail.com>
2024-08-20 23:13:43 -0700

    Clean up

--- pyBulletSimImporter.py
+++ pyBulletSimImporter.py
@@ -1,10 +1,65 @@
 import pickle
-from os.path import basename, join, splitext
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List, Tuple
 
 import bpy
 from bpy.props import CollectionProperty, StringProperty
 from bpy.types import Operator, OperatorFileListElement, Panel
 from bpy_extras.io_utils import ImportHelper
+
+
+@dataclass
+class Frame:
+    position: Tuple[float, float, float]  # xyz
+    orientation: Tuple[float, float, float, float]  # xyzw
+
+    def __post_init__(self):
+        assert len(self.position) == 3, f"len(self.position): {len(self.position)}"
+        assert (
+            len(self.orientation) == 4
+        ), f"len(self.orientation): {len(self.orientation)}"
+
+    @classmethod
+    def from_dict(cls, data: Dict):
+        return cls(position=data["position"], orientation=data["orientation"])
+
+    def to_dict(self):
+        return {"position": self.position, "orientation": self.orientation}
+
+
+@dataclass
+class Trajectory:
+    type: str
+    mesh_path: Path
+    mesh_scale: Tuple[float, float, float]
+    frames: List[Frame]
+
+    def __post_init__(self):
+        assert self.type in ["mesh"], f"self.type: {self.type}"
+        assert self.mesh_path.exists(), f"self.mesh_path: {self.mesh_path}"
+        assert (
+            len(self.mesh_scale) == 3
+        ), f"len(self.mesh_scale): {len(self.mesh_scale)}"
+        assert len(self.frames) > 0, f"len(self.frames): {len(self.frames)}"
+
+    @classmethod
+    def from_dict(cls, data: Dict):
+        return cls(
+            type=data["type"],
+            mesh_path=Path(data["mesh_path"]),
+            mesh_scale=data["mesh_scale"],
+            frames=[Frame.from_dict(frame) for frame in data["frames"]],
+        )
+
+    def to_dict(self):
+        return {
+            "type": self.type,
+            "mesh_path": str(self.mesh_path),
+            "mesh_scale": self.mesh_scale,
+            "frames": [frame.to_dict() for frame in self.frames],
+        }
+
 
 bl_info = {
     "name": "PyBulletSimImporter",
@@ -35,40 +90,41 @@ class ANIM_OT_import_pybullet_sim(Operator, ImportHelper):
 
     def execute(self, context):
         for file in self.files:
-            filepath = join(self.directory, file.name)
+            filepath = Path(self.directory) / file.name
             print(f"Processing {filepath}")
+
             with open(filepath, "rb") as pickle_file:
-                data = pickle.load(pickle_file)
-                collection_name = splitext(basename(filepath))[0]
-                collection = bpy.data.collections.new(collection_name)
+                raw_data = pickle.load(pickle_file)
+                trajectories = {
+                    link_name: Trajectory.from_dict(data_dict)
+                    for link_name, data_dict in raw_data.items()
+                }
+
+                collection = bpy.data.collections.new(filepath.stem)
                 bpy.context.scene.collection.children.link(collection)
                 context.view_layer.active_layer_collection = (
                     context.view_layer.layer_collection.children[-1]
                 )
 
-                for obj_key in data:
-                    pybullet_obj = data[obj_key]
-
+                for link_name, trajectory in trajectories.items():
                     # Load mesh of each link
-                    assert pybullet_obj["type"] == "mesh"
-                    extension = pybullet_obj["mesh_path"].split(".")[-1].lower()
+                    extension = trajectory.mesh_path.suffix
+
                     # Handle different mesh formats
                     if "obj" in extension:
                         bpy.ops.wm.obj_import(
-                            filepath=pybullet_obj["mesh_path"],
+                            filepath=trajectory.mesh_path,
                             forward_axis="Y",
                             up_axis="Z",
                         )
                     elif "dae" in extension:
-                        bpy.ops.wm.collada_import(filepath=pybullet_obj["mesh_path"])
+                        bpy.ops.wm.collada_import(filepath=trajectory.mesh_path)
                     elif "stl" in extension:
-                        bpy.ops.wm.stl_import(filepath=pybullet_obj["mesh_path"])
+                        bpy.ops.wm.stl_import(filepath=trajectory.mesh_path)
                     else:
                         print("Unsupported File Format:{}".format(extension))
                         pass
 
-                    # Delete lights and camera
-                    parts = 0
                     final_objs = []
                     for import_obj in context.selected_objects:
                         bpy.ops.object.select_all(action="DESELECT")
@@ -78,51 +134,56 @@ class ANIM_OT_import_pybullet_sim(Operator, ImportHelper):
                             or "Light" in import_obj.name
                             or "Lamp" in import_obj.name
                         ):
+                            # Delete lights and camera
                             bpy.ops.object.delete(use_global=True)
                         else:
-                            scale = pybullet_obj["mesh_scale"]
+                            # Set scale
+                            scale = trajectory.mesh_scale
                             if scale is not None:
                                 import_obj.scale.x = scale[0]
                                 import_obj.scale.y = scale[1]
                                 import_obj.scale.z = scale[2]
                             final_objs.append(import_obj)
-                            parts += 1
+
                     bpy.ops.object.select_all(action="DESELECT")
                     for obj in final_objs:
                         if obj.type == "MESH":
                             obj.select_set(True)
+
                     if len(context.selected_objects):
                         context.view_layer.objects.active = context.selected_objects[0]
                         # join them
                         bpy.ops.object.join()
+
                     blender_obj = context.view_layer.objects.active
-                    blender_obj.name = obj_key
+                    blender_obj.name = link_name
 
                     # Keyframe motion of imported object
-                    for frame_count, frame_data in enumerate(pybullet_obj["frames"]):
-                        if frame_count % self.skip_frames != 0:
+                    for frame_i, frame in enumerate(trajectory.frames):
+                        if frame_i % self.skip_frames != 0:
                             continue
-                        if self.max_frames > 1 and frame_count > self.max_frames:
+
+                        if self.max_frames > 1 and frame_i > self.max_frames:
                             print("Exceed max frame count")
                             break
-                        percentage_done = frame_count / len(pybullet_obj["frames"])
+
+                        percentage_done = frame_i / len(trajectory.frames)
                         print(
-                            f"\r[{percentage_done*100:.01f}% | {obj_key}]",
+                            f"\r[{percentage_done*100:.01f}% | {link_name}]",
                             "#" * int(60 * percentage_done),
                             end="",
                         )
-                        pos = frame_data["position"]
-                        orn = frame_data["orientation"]
-                        context.scene.frame_set(frame_count // self.skip_frames)
+                        context.scene.frame_set(frame_i // self.skip_frames)
+
                         # Apply position and rotation
-                        blender_obj.location.x = pos[0]
-                        blender_obj.location.y = pos[1]
-                        blender_obj.location.z = pos[2]
+                        blender_obj.location.x = frame.position[0]
+                        blender_obj.location.x = frame.position[1]
+                        blender_obj.location.x = frame.position[2]
                         blender_obj.rotation_mode = "QUATERNION"
-                        blender_obj.rotation_quaternion.x = orn[0]
-                        blender_obj.rotation_quaternion.y = orn[1]
-                        blender_obj.rotation_quaternion.z = orn[2]
-                        blender_obj.rotation_quaternion.w = orn[3]
+                        blender_obj.rotation_quaternion.x = frame.orientation[0]
+                        blender_obj.rotation_quaternion.y = frame.orientation[1]
+                        blender_obj.rotation_quaternion.z = frame.orientation[2]
+                        blender_obj.rotation_quaternion.w = frame.orientation[3]
                         bpy.ops.anim.keyframe_insert_menu(type="Rotation")
                         bpy.ops.anim.keyframe_insert_menu(type="Location")
         return {"FINISHED"}
